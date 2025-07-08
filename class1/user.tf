b97b722b28161e06ae51610a80ea86c0857ef6f1
IsmayilJabrayilov <ismayil.cabrayilovv@gmail.com>
2025-07-08 01:57:33 +0000

    class 1

---
+++ class1/user.tf
@@ -0,0 +1,15 @@
+resource "aws_iam_user" "lb" {
+  name = "loadbalancer"
+}
+
+resource "aws_iam_user" "lb2" {
+  name = "loadbalancer2"
+}
+
+resource "aws_iam_user" "lb3" {
+  name = "loadbalancer3"
+}
+
+resource "aws_iam_user" "lb4" {
+  name = "kaizen"
+}
---
+++ class1/user_to_group.tf
@@ -0,0 +1,11 @@
+resource "aws_iam_group_membership" "team" {
+  name = "tf-testing-group-membership"
+
+  users = [
+    aws_iam_user.lb.name,
+    aws_iam_user.lb2.name
+  ]
+
+  group = aws_iam_group.developers.name
+
+}
